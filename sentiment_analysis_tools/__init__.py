9241883be256637405d537ec7df06d0c17a5e99f
Ali Abbas <v2thegreat@gmail.com>
2020-07-05 16:59:23 -0400

    Added overall setup for using libraries

---
+++ sentiment_analysis_tools/__init__.py
@@ -0,0 +1,55 @@
+"""
+Author: v2thegreat
+
+Libraries Used:
+ - NLTK
+ - SpaCy
+ - TextBlob
+ - Stanford CoreNLP
+ - Gensim
+"""
+
+
+def sentiment_analysis_nltk(text: str) -> float:
+    """
+    Run sentiment analysis using the library NLTK. Returns default sentiment
+    :param text:
+    :return: sentiment for given text
+    """
+    return 0.0
+
+
+def sentiment_analysis_spacy(text: str) -> float:
+    """
+    Run sentiment analysis using the library NLTK. Returns default sentiment
+    :param text: text to be analysed
+    :return: sentiment for given text
+    """
+    return 0.0
+
+
+def sentiment_analysis_textblob(text: str) -> float:
+    """
+    Run sentiment analysis using the library NLTK. Returns default sentiment
+    :param text: text to be analysed
+    :return: sentiment for given text
+    """
+    return 0.0
+
+
+def sentiment_analysis_stanford_core_nlp(text: str) -> float:
+    """
+    Run sentiment analysis using the library NLTK. Returns default sentiment
+    :param text: text to be analysed
+    :return: sentiment for given text
+    """
+    return 0.0
+
+
+def sentiment_analysis_gensim(text: str) -> float:
+    """
+    Run sentiment analysis using the library NLTK. Returns default sentiment
+    :param text: text to be analysed
+    :return: sentiment for given text
+    """
+    return 0.0
