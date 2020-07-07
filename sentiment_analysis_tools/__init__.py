6af4e086689c325bc090c34876d356dabd247032
Ali Abbas <v2thegreat@gmail.com>
2020-07-06 23:15:33 -0400

    Added additional docs explaining how different models are different from each other

--- sentiment_analysis_tools/__init__.py
+++ sentiment_analysis_tools/__init__.py
@@ -26,7 +26,9 @@ Libraries Used:
 def sentiment_analysis_nltk(text: str) -> float:
     """
     Run sentiment analysis using the library NLTK. Runs default sentiment on vader lexicon
-    :param text:
+
+    Works based on bag of words and positive and negative word lookups
+    :param text: text to be analysed
     :return: sentiment compound for given text
     """
     return NLTK_SENTIMENT_INTENSITY_ANALYZER.polarity_scores(text=text)['compound']
@@ -35,6 +37,8 @@ def sentiment_analysis_nltk(text: str) -> float:
 def sentiment_analysis_textblob(text: str) -> float:
     """
     Run sentiment analysis using the library textblob. Returns default sentiment
+
+    Works similar to NLTK's sentiment analysis, but includes subjectivity analysis
     :param text: text to be analysed
     :return: sentiment for given text
     """
@@ -44,6 +48,8 @@ def sentiment_analysis_textblob(text: str) -> float:
 def sentiment_analysis_flair(text: str) -> float:
     """
     Run sentiment analysis using the library flair. Returns default sentiment
+
+    Works based on a character-level LSTM neural network
     :param text: text to be analysed
     :return: sentiment for given text
     """
