d2c4e853cbd1c81d6b2ffa6b00018cef3b3b03e6
Ali Abbas <v2thegreat@gmail.com>
2020-07-09 22:00:59 -0400

    Added initial functionality to get sentiments from tweets

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+import pandas as pd
+from tqdm import tqdm
+
+from sentiment_analysis_tools import Sentiments
+
+tweets_dict = {}
+
+dataframe = pd.Dataframe(columns=['topic_name', 'tweets'])
+
+for topic in tqdm(tweets_dict.keys()):
+    tweets = pd.Series(tweets_dict[topic])
+    topics = pd.Series([topic] * tweets.size)
+    dataframe.append((topics, tweets))
+
+sentiments = [ Sentiments.multiple_sentiment_analysis(tweet) for tweet in dataframe.tweets.iterrows()]
+
+dataframe = dataframe.concat(sentiments)
 
 if __name__ == "__main__":
-    raise NotImplementedError('Nothing has been built yet!')
+    print(dataframe)
