d1f892419edb2858b15fab847282ec422a2c2c9d
Ali Abbas <v2thegreat@gmail.com>
2020-07-15 22:22:12 -0400

    Small modifications

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ tweets_dict = {'abcd': ['The world is not a good place', 'I hate you!']}
 all_topics = []
 all_tweets = []
 
-for topic in tqdm(tweets_dict.keys()):
+for topic in tweets_dict.keys():
     all_topics.extend([topic] * len(tweets_dict[topic]))
     all_tweets.extend(tweets_dict[topic])
 
 dataframe = {'topic_name': all_topics, 'tweets': all_tweets}
 
-sentiments = pd.DataFrame([Sentiments.multiple_sentiment_analysis(tweet) for tweet in dataframe['tweets']])
+sentiments = pd.DataFrame([Sentiments.multiple_sentiment_analysis(tweet) for tweet in tqdm(dataframe['tweets'])])
 
 sentiments['tweets'] = dataframe['tweets']
 sentiments['topic_name'] = dataframe['topic_name']
