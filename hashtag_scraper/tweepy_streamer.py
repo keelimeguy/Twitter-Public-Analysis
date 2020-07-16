43e8ddc318944621f48abfeafc5462f0be6e1368
Ali Abbas <v2thegreat@gmail.com>
2020-07-15 22:19:35 -0400

    Added cleanup as mentioned in #6

--- hashtag_scraper/tweepy_streamer.py
+++ hashtag_scraper/tweepy_streamer.py
@@ -7,7 +7,7 @@ import re
 from hashtag_scraper import twitter_credentials
 
 
-def clean_tweet(tweet):
+def _clean_tweet(tweet):
     """
     Cleans the tweet using regular expression for special characters or hyperlinks.
 
@@ -17,13 +17,14 @@ def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z\t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 
-def fetch_tweets(num_topics, num_tweets, geo_code=2388929, clean_bool=True):
+def fetch_tweets(num_topics, num_tweets, geo_code=2388929, clean_tweets=True):
     """
     Fetches a number of tweets on a specified number of trending topics using a particular geo code.
 
     :param num_topics: Number of treading topics to be collected.
     :param num_tweets: Number of tweets per topic to be collected.
     :param geo_code: Geo code to get trending topics for that area.
+    :param clean_tweets: Should we automatically clean the tweet?
     :return: Fetches a number of tweets on a specified number of tweets per a specified number of topics from a list of trending topics at a location.
     """
     # Creating an instance of
@@ -51,15 +52,15 @@ def fetch_tweets(num_topics, num_tweets, geo_code=2388929, clean_bool=True):
 
     # Nest loop for searching each trending topic and then inserting those tweets into 'trending' dict
     for i in names:
-        temp = api.search(q=i, count=num_tweets+1)
+        temp = api.search(q=i, count=num_tweets + 1)
 
         # Debug => prints text content of tweet of specified index
         # print(temp[0].text)
 
         for j in range(0, num_tweets):
-            if clean_bool == True:
-                tweets.append(clean_tweet(temp[j].text))
-            elif clean_bool == False:
+            if clean_tweets:
+                tweets.append(_clean_tweet(temp[j].text))
+            elif not clean_tweets:
                 tweets.append(temp[j].text)
             else:
                 tweets.append(temp[j].text)
@@ -69,12 +70,10 @@ def fetch_tweets(num_topics, num_tweets, geo_code=2388929, clean_bool=True):
     print(trending)
 
 
-"""
-This class contains basic functions to authenticate using the TwitterAuthenticator class. 
-"""
-
-
-class TwitterClient():
+class TwitterClient:
+    """
+    This class contains basic functions to authenticate using the TwitterAuthenticator class.
+    """
 
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
@@ -90,52 +89,49 @@ class TwitterClient():
         return self.twitter_client
 
 
-"""
-This class contains a function to fetch authentication keys from 'twitter_credentials.py' and serve along side the TwitterClient class.
-"""
+class TwitterAuthenticator:
+    """
+    This class contains a function to fetch authentication keys from 'twitter_credentials.py' and serve along side the TwitterClient class.
+    """
 
-
-class TwitterAuthenticator():
-
-    def authenticate_twitter_app(self):
+    @classmethod
+    def authenticate_twitter_app(cls):
         """
         A modular function that gets called on to authenticate to Twitter API
 
         :return: Fetches Twitter API keys from twitter_credentials and uses OAuthHandler from tweepy library to authenticate Twitter API.
         """
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN,
+                              twitter_credentials.ACCESS_TOKEN_SECRET)
         return auth
 
 
-"""
-This class automatically passes the TwitterAuthenticator and TwitterListener to connect to the Twitter API.
-"""
-
-
-class TwitterStreamer():
+class TwitterStreamer:
+    """
+    This class automatically passes the TwitterAuthenticator and TwitterListener to connect to the Twitter API.
+    """
 
     def __init__(self):
         self.twitter_authenticator = TwitterAuthenticator
 
-    def stream_tweets(self, fetched_tweet_filename, hash_tag_list):
+    def stream_tweets(self, hash_tag_list):
         # This handles Twitter authentication and the connection to the Twitter Streaming API.
         listener = TwitterListener()
-        auth = self.twitter_authenticator.authenticate_twiiter_app()
+        auth = self.twitter_authenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
 
         # this filters tweets, inside track can be a list of stuff to include inside the listener
         stream.filter(track=hash_tag_list)
 
 
-"""
-This class listens to the response from the Twitter API for any important codes or exceptions that are raised. 
-"""
-
-
 class TwitterListener(StreamListener):
+    """
+    This class listens to the response from the Twitter API for any important codes or exceptions that are raised.
+    """
 
     def __init__(self, fetched_tweets_filename):
+        super().__init__()
         self.fetched_tweets_filename = fetched_tweets_filename
 
     def on_data(self, data):
@@ -167,3 +163,7 @@ class TwitterListener(StreamListener):
             return False
 
         print(status)
+
+
+if __name__ == '__main__':
+    pass
