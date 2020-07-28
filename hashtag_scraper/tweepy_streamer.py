71f8ce08eae5adca6292a493f5ffe608538db3ff
Ali Abbas <v2thegreat@gmail.com>
2020-07-28 01:01:41 -0400

    Refactored to make code easier to work with and with better encapsulation. Also added (slightly) more functionality

--- hashtag_scraper/tweepy_streamer.py
+++ hashtag_scraper/tweepy_streamer.py
@@ -1,82 +1,18 @@
+import re
 from tweepy import API
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-import re
-
-from hashtag_scraper import twitter_credentials
+from config import config
 
 
-def _clean_tweet(tweet):
-    """
-    Cleans the tweet using regular expression for special characters or hyperlinks.
-
-    :param tweet: text to be processed
-    :return: text without special characters or hyperlinks
-    """
-    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z\t])|(\w+:\/\/\S+)", " ", tweet).split())
-
-
-def fetch_tweets(num_topics, num_tweets, geo_code=2388929, clean_tweets=True):
-    """
-    Fetches a number of tweets on a specified number of trending topics using a particular geo code.
-
-    :param num_topics: Number of treading topics to be collected.
-    :param num_tweets: Number of tweets per topic to be collected.
-    :param geo_code: Geo code to get trending topics for that area.
-    :param clean_tweets: Should we automatically clean the tweet?
-    :return: Fetches a number of tweets on a specified number of tweets per a specified number of topics from a list of trending topics at a location.
-    """
-    # Creating an instance of
-    twitter_client = TwitterClient()
-    api = twitter_client.get_twitter_client_api()
-
-    # Get trending topics per Geo location ID
-    trends1 = api.trends_place(id=geo_code)
-    data = trends1[0]
-
-    # grab the trends
-    trends = data['trends']
-
-    # grab the name from each trend
-    names = [trend['name'] for trend in trends[:num_topics]]
-
-    # Debug => prints trending list
-    # print(names)
-
-    # number of tweets to be gathered per trending topic
-
-    # Initialize dict for trending topic and respective tweets
-    trending = {}
-    tweets = []
-
-    # Nest loop for searching each trending topic and then inserting those tweets into 'trending' dict
-    for i in names:
-        temp = api.search(q=i, count=num_tweets + 1)
-
-        # Debug => prints text content of tweet of specified index
-        # print(temp[0].text)
-
-        for j in range(0, num_tweets):
-            if clean_tweets:
-                tweets.append(_clean_tweet(temp[j].text))
-            elif not clean_tweets:
-                tweets.append(temp[j].text)
-            else:
-                tweets.append(temp[j].text)
-
-        trending[i] = tweets
-
-    print(trending)
-
-
-class TwitterClient:
+class _TwitterClient:
     """
     This class contains basic functions to authenticate using the TwitterAuthenticator class.
     """
 
     def __init__(self, twitter_user=None):
-        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.auth = _TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
         self.twitter_user = twitter_user
 
@@ -89,7 +25,7 @@ class TwitterClient:
         return self.twitter_client
 
 
-class TwitterAuthenticator:
+class _TwitterAuthenticator:
     """
     This class contains a function to fetch authentication keys from 'twitter_credentials.py' and serve along side the TwitterClient class.
     """
@@ -101,23 +37,23 @@ class TwitterAuthenticator:
 
         :return: Fetches Twitter API keys from twitter_credentials and uses OAuthHandler from tweepy library to authenticate Twitter API.
         """
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN,
-                              twitter_credentials.ACCESS_TOKEN_SECRET)
+        auth = OAuthHandler(config.API_KEY, config.API_SECRET_KEY)
+        auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+
         return auth
 
 
-class TwitterStreamer:
+class _TwitterStreamer:
     """
     This class automatically passes the TwitterAuthenticator and TwitterListener to connect to the Twitter API.
     """
 
     def __init__(self):
-        self.twitter_authenticator = TwitterAuthenticator
+        self.twitter_authenticator = _TwitterAuthenticator
 
     def stream_tweets(self, hash_tag_list):
         # This handles Twitter authentication and the connection to the Twitter Streaming API.
-        listener = TwitterListener()
+        listener = _TwitterListener()
         auth = self.twitter_authenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
 
@@ -125,7 +61,7 @@ class TwitterStreamer:
         stream.filter(track=hash_tag_list)
 
 
-class TwitterListener(StreamListener):
+class _TwitterListener(StreamListener):
     """
     This class listens to the response from the Twitter API for any important codes or exceptions that are raised.
     """
@@ -165,5 +101,67 @@ class TwitterListener(StreamListener):
         print(status)
 
 
+def _clean_tweet(tweet):
+    """
+    Cleans the tweet using regular expression for special characters or hyperlinks.
+
+    :param tweet: text to be processed
+    :return: text without special characters or hyperlinks
+    """
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z\t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+
+def fetch_tweets(num_topics: int, num_tweets: int, geo_code: int = 2388929,
+                 clean_tweets: bool = True):
+    """
+    Fetches a number of tweets on a specified number of trending topics using a particular geo code.
+
+    :param num_topics: Number of treading topics to be collected.
+    :param num_tweets: Number of tweets per topic to be collected.
+    :param geo_code: Geo code to get trending topics for that area.
+    :param clean_tweets: Should we automatically clean the tweet?
+    :return: Fetches a number of tweets on a specified number of tweets per a specified number of topics from a list of trending topics at a location.
+    """
+    # Creating an instance of
+    twitter_client = _TwitterClient()
+    api = twitter_client.get_twitter_client_api()
+
+    # Get trending topics per Geo location ID
+    trends_place = api.trends_place(id=geo_code)
+    data = trends_place[0]
+
+    # grab the trends
+    trends = data['trends']
+
+    # grab the name from each trend
+    names = [trend['name'] for trend in trends[:num_topics]]
+
+    # Debug => prints trending list
+    # print(names)
+
+    # number of tweets to be gathered per trending topic
+
+    # Initialize dict for trending topic and respective tweets
+    trending = {}
+    tweets = []
+
+    # Nest loop for searching each trending topic and then inserting those tweets into 'trending' dict
+    for name in names:
+        temp = api.search(q=name, count=num_tweets + 1)
+
+        # Debug => prints text content of tweet of specified index
+        # print(temp[0].text)
+
+        for j in range(num_tweets):
+            if clean_tweets:
+                tweets.append(_clean_tweet(temp[j].text))
+            else:
+                tweets.append(temp[j].text)
+
+        trending[name] = tweets
+
+    return trending
+
+
 if __name__ == '__main__':
-    pass
+    print(fetch_tweets(4, 100))
