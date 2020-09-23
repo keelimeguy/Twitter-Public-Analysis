628f02437df675f752e12c71f2debaf7c67f0c47
Keelin Wheeler <keelin.wheeler@gmail.com>
2020-09-22 23:31:21 -0400

    fix(tests): using standard cases in file structure

--- tests/test_downloader_tools.py
+++ tests/test_downloader_tools.py
@@ -1,6 +1,6 @@
 import unittest
 
-from Dozent.DownloaderTools import DownloaderTools
+from dozent.downloader_tools import DownloaderTools
 
 from pySmartDL import SmartDL
 from pySmartDL.control_thread import ControlThread
