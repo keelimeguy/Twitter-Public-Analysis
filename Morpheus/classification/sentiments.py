e6995cbceb9f9cb10ac02fe070a16e06532b8300
v2thegreat <v2thegreat@gmail.com>
2020-09-23 20:24:32 -0400

    Removed flair as a dependency throught out the entire project

--- Morpheus/classification/sentiments.py
+++ Morpheus/classification/sentiments.py
@@ -1,17 +1,14 @@
 from typing import Dict
 
+import math
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from textblob import TextBlob
-
-nltk.download('vader_lexicon', quiet=True)
-NLTK_SENTIMENT_INTENSITY_ANALYZER = SentimentIntensityAnalyzer()
+from pprint import pprint
 
 """
 Author: v2thegreat
-
-Tutorial Followed: https://medium.com/@b.terryjack/nlp-pre-trained-sentiment-analysis-1eb52a9d742c
-
+Source: https://medium.com/@b.terryjack/nlp-pre-trained-sentiment-analysis-1eb52a9d742c
 Libraries Used:
  - NLTK
  - TextBlob
@@ -20,62 +17,88 @@ Libraries Used:
 
 
 class Sentiments:
-    @classmethod
-    def sentiment_analysis_nltk(cls, text: str) -> float:
+    __instance__ = None
+
+    def __init__(self):
+        if Sentiments.__instance__ is None:
+            Sentiments.__instance__ = self
+
+            nltk.download('vader_lexicon', quiet=True)
+            self.NLTK_SENTIMENT_INTENSITY_ANALYZER = SentimentIntensityAnalyzer()
+
+            # Depreciated due to dependency issues
+            # self.flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+
+        else:
+            raise RuntimeError("singleton Sentiments class is created more than once!")
+
+    @staticmethod
+    def get_instance():
+        """Returns a singleton instance of this Sentiments class
+        """
+        if not Sentiments.__instance__:
+            Sentiments()
+        return Sentiments.__instance__
+
+    @staticmethod
+    def sentiment_analysis_nltk(text: str) -> float:
         """
         Run sentiment analysis using the library NLTK. Runs default sentiment on vader lexicon
-
         Works based on bag of words and positive and negative word lookups
-        :param text: text to be analysed
+        :param text: text to be analyzed
         :return: sentiment compound for given text
         """
-        return NLTK_SENTIMENT_INTENSITY_ANALYZER.polarity_scores(text=text)['compound']
+        if not text:
+            return math.nan
 
-    @classmethod
-    def sentiment_analysis_textblob(cls, text: str) -> float:
+        return Sentiments.get_instance().NLTK_SENTIMENT_INTENSITY_ANALYZER.polarity_scores(text=text)['compound']
+
+    @staticmethod
+    def sentiment_analysis_textblob(text: str) -> float:
         """
         Run sentiment analysis using the library textblob. Returns default sentiment
-
         Works similar to NLTK's sentiment analysis, but includes subjectivity analysis
-        :param text: text to be analysed
+        :param text: text to be analyzed
         :return: sentiment for given text
         """
+        if not text:
+            return math.nan
+
         return TextBlob(text=text).sentiment.polarity
 
-    @classmethod
-    def sentiment_analysis_flair(cls, text: str) -> float:
+    @staticmethod
+    def sentiment_analysis_flair(text: str) -> float:
         """
         Run sentiment analysis using the library flair. Returns default sentiment
-
         Works based on a character-level LSTM neural network
-        :param text: text to be analysed
+        :param text: text to be analyzed
         :return: sentiment for given text
         """
-        raise DeprecationWarning("This function has been depreciated")
+
+        raise DeprecationWarning("Depreciated due to dependency issues. Do not use")
+        #
         # if not text:
-        #     return 0
+        #     return math.nan
         #
         # s = flair.data.Sentence(text)
-        # flair_sentiment.predict(s)
+        # Sentiments.get_instance().flair_sentiment.predict(s)
         # total_sentiment = s.labels
         # if total_sentiment[0].value == 'NEGATIVE':
         #     return total_sentiment[0].score * -1
         # else:
         #     return total_sentiment[0].score
 
-    @staticmethod
-    def multiple_sentiment_analysis(text: str) -> Dict[str, float]:
-        return {sentiment: _sentiment_functions[sentiment](text) for sentiment in
-                _sentiment_functions.keys()}
+    _sentiment_function_mapping = {
+        'nltk': sentiment_analysis_nltk.__func__,
+        'textblob': sentiment_analysis_textblob.__func__
+    }
 
+    @classmethod
+    def multiple_sentiment_analysis(cls, text: str) -> Dict[str, float]:
+        return {sentiment: sentiment_function(text) for sentiment, sentiment_function in
+                cls._sentiment_function_mapping.items()}
 
-_sentiment_functions = {
-    'nltk': Sentiments.sentiment_analysis_nltk,
-    'textblob': Sentiments.sentiment_analysis_textblob
-}
 
 if __name__ == "__main__":
-    sentence = 'The world is not a good place'
-    print(Sentiments.sentiment_analysis_nltk(sentence))
-    print(Sentiments.sentiment_analysis_textblob(sentence))
-    # print(Sentiments.sentiment_analysis_flair(sentence))
+    _sentence = 'The world is not a good place'
+    pprint(Sentiments.multiple_sentiment_analysis(_sentence))
