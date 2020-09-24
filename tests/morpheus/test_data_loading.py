c5c2afc0c87d8ee22540963f05001744190b3695
v2thegreat <v2thegreat@gmail.com>
2020-09-24 00:40:06 -0400

    Modified because order doesn't matter at this time

--- tests/morpheus/test_data_loading.py
+++ tests/morpheus/test_data_loading.py
@@ -15,39 +15,39 @@ class DataLoadingTestCase(unittest.TestCase):
             If order matters, glob might have to be reconfigured
         :return:
         """
-        self.assertEqual(
-            set(DataLoading.get_files_list(self.data_path)),
-            {f'{self.path_prefix}\\test_sample_files.json.bz2', f'{self.path_prefix}\\test_sample_files_2.json.bz2'})
+        self.assertTrue(
+            set(DataLoading.get_files_list(self.data_path)) == {f'{self.path_prefix}\\test_sample_files.json.bz2',
+                                                                f'{self.path_prefix}\\test_sample_files_2.json.bz2'})
 
-    def test_get_files_list_when_no_files_present(self):
-        try:
-            DataLoading.get_files_list('../../data/no-files.abcd')
-            self.assertFalse(False)
-        except ValueError:
-            self.assertTrue(True)
+        def test_get_files_list_when_no_files_present(self):
+            try:
+                DataLoading.get_files_list('../../data/no-files.abcd')
+                self.assertFalse(False)
+            except ValueError:
+                self.assertTrue(True)
 
-    def test_read_compressed_bz2_json_file(self):
-        data = DataLoading.read_compressed_bz2_json_file(f'{self.path_prefix}/test_sample_files.json.bz2')
-        self.assertEqual(type(data), list)
+        def test_read_compressed_bz2_json_file(self):
+            data = DataLoading.read_compressed_bz2_json_file(f'{self.path_prefix}/test_sample_files.json.bz2')
+            self.assertEqual(type(data), list)
 
-    def test_read_compressed_bz2_json_file_when_not_json_bz2_file(self):
-        try:
-            DataLoading.read_compressed_bz2_json_file('../../data/test_sample_files.json')
-            self.assertFalse(False)
-        except ValueError:
-            self.assertTrue(True)
+        def test_read_compressed_bz2_json_file_when_not_json_bz2_file(self):
+            try:
+                DataLoading.read_compressed_bz2_json_file('../../data/test_sample_files.json')
+                self.assertFalse(False)
+            except ValueError:
+                self.assertTrue(True)
 
-    def test_get_twitter_data_as_bags(self):
-        data = DataLoading.get_twitter_data_as_bags('../../data/test_sample_files.json.bz2')
-        self.assertEqual(type(data), Bag)
+        def test_get_twitter_data_as_bags(self):
+            data = DataLoading.get_twitter_data_as_bags('../../data/test_sample_files.json.bz2')
+            self.assertEqual(type(data), Bag)
 
-    def test_remove_deleted_tweets(self):
-        """
-        Note: Function assumes that there is a removed tweet in the first 20 tweets.
-        :return:
-        """
-        bags = DataLoading.get_twitter_data_as_bags(self.data_path, remove_deleted_tweets=False)
-        removed_tweets = DataLoading.remove_deleted_tweets(bags)
-        bags = bags.take(20)
-        computed = removed_tweets.take(20)
-        self.assertNotEqual(bags, computed)
+        def test_remove_deleted_tweets(self):
+            """
+            Note: Function assumes that there is a removed tweet in the first 20 tweets.
+            :return:
+            """
+            bags = DataLoading.get_twitter_data_as_bags(self.data_path, remove_deleted_tweets=False)
+            removed_tweets = DataLoading.remove_deleted_tweets(bags)
+            bags = bags.take(20)
+            computed = removed_tweets.take(20)
+            self.assertNotEqual(bags, computed)
